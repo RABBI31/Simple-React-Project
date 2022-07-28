293f3ce95d654c77de0030de8f0c8be8887f8179
rabbi31 <18103380@iubat.edu>
2022-07-28 23:36:43 +0600

    code format

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-import Main from "./componants/Main"
-import About from "./componants/About";
-import Services from "./componants/Services";
-import Blog from "./componants/Blog";
-import Contact from "./componants/Contact"
-import './App.css';
+import { Navigate, Route, Routes } from 'react-router-dom'
+import Main from './componants/Main'
+import About from './componants/About'
+import Services from './componants/Services'
+import Blog from './componants/Blog'
+import Contact from './componants/Contact'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-       <Routes>
-       <Route path="/" element={<Main />} />
-       <Route path="/about" element={<About />} />
-       <Route path="/services" element={<Services />} />
-       <Route path="/blog" element={<Blog />} />
-       <Route path="/contact" element={<Contact />} />
-       </Routes>
-      
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/blog' element={<Blog />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
